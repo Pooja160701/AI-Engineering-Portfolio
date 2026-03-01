d29adff7d9d5328a0888ecaffa4d2e3393aa5e49
Pooja160701 <poojaajithan160701@gmail.com>
2026-03-01 08:35:33 +0530

    Fix model loading for CI environment

--- MLOps/churn-prediction-mlops/app/main.py
+++ MLOps/churn-prediction-mlops/app/main.py
@@ -2,35 +2,52 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import numpy as np
-from src.utils.logger import get_logger
 from fastapi import HTTPException
+from src.utils.logger import get_logger
+import os
 
 logger = get_logger()
 
 app = FastAPI(title="Churn Prediction API")
 
-model = joblib.load("artifacts/model.pkl")
+model = None  # global placeholder
+
+
+@app.on_event("startup")
+def load_model():
+    global model
+    model_path = "artifacts/model.pkl"
+
+    if os.path.exists(model_path):
+        model = joblib.load(model_path)
+        logger.info("Model loaded successfully.")
+    else:
+        logger.warning("Model file not found. Running without loaded model.")
+
 
 class InputData(BaseModel):
     feature1: float
     feature2: float
 
+
 @app.get("/")
 def root():
     return {"message": "Churn Prediction API is live"}
+
 
 @app.get("/health")
 def health():
     return {"status": "API is running"}
 
+
 @app.post("/predict")
 def predict(data: InputData):
-    try:
-        logger.info("Prediction request received")
+    if model is None:
+        raise HTTPException(status_code=503, detail="Model not loaded")
 
+    try:
         input_array = np.array([[data.feature1, data.feature2]])
         prediction = model.predict(input_array)[0]
-
         return {"prediction": int(prediction)}
 
     except Exception as e:
